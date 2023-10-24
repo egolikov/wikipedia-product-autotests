d73312dcb798b1a1991630d6b624b63fda6c1a73
egolikov <panda_93a@mail.ru>
2023-10-25 01:26:33 +0600

    * new component

--- src/test/java/qa/guru/pages/components/NavTabComponents.java
+++ src/test/java/qa/guru/pages/components/NavTabComponents.java
@@ -7,10 +7,39 @@ import static io.appium.java_client.AppiumBy.id;
 
 public class NavTabComponents {
 
-    private final SelenideElement searchComponentButton = $(id("org.wikipedia.alpha:id/nav_tab_search"));
+    private final SelenideElement
+            exploreComponentButton = $(id("org.wikipedia.alpha:id/nav_tab_explore")),
+            savedComponentButton = $(id("org.wikipedia.alpha:id/nav_tab_reading_lists")),
+            searchComponentButton = $(id("org.wikipedia.alpha:id/nav_tab_search")),
+            editsComponentButton = $(id("org.wikipedia.alpha:id/nav_tab_edits")),
+            moreComponentButton = $(id("org.wikipedia.alpha:id/nav_more_container"));
 
-    public NavTabComponents clickSearchComponentButton() {
+    public NavTabComponents clickExploreComponentButton() {
+        exploreComponentButton.click();
+
+        return this;
+    }
+
+    public NavTabComponents clickSavedComponentButton() {
+        savedComponentButton.click();
+
+        return this;
+    }
+
+    public NavTabComponents clickSearchComponentButton() { //todo may be void method ?
         searchComponentButton.click();
+
+        return this;
+    }
+
+    public NavTabComponents clickEditsComponentButton() {
+        editsComponentButton.click();
+
+        return this;
+    }
+
+    public NavTabComponents clickMoreComponentButton() {
+        moreComponentButton.click();
 
         return this;
     }
